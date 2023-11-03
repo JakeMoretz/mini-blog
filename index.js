c9117f64ee6dc9b4dba9d1251684d37110b1d56a
Jake Moretz <jmoretz9090@gmail.com>
2023-11-02 22:59:20 -0400

    started hooking up to the database

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-const publishBtn = document.querySelector(".publish-btn")
-const postSection = document.querySelector(".container")
-const postInput = document.getElementById("input-field")
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js';
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+} from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js';
 
-function addPost() {
-    let newEl = document.createElement('p')
-    newEl.textContent = document.getElementById("input-field").value
-    newEl.className = "post-container"
-    postSection.appendChild(newEl)
+const appSettings = {
+    databaseURL: 'https://thinkful-88088-default-rtdb.firebaseio.com/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const blogPostInDb = ref(database, 'blogList');
+
+const publishBtn = document.querySelector('.publish-btn');
+const postSection = document.querySelector('.container');
+const postInput = document.getElementById('input-field');
+
+onValue(blogPostInDb, function (snapshot) {
+    //turn object into array
+    let blogPostArray = Object.values(snapshot.val());
+    clearElement();
+    //loop through the array
+    for (let i = 0; i < blogPostArray.length; i++) {
+        let currentPost = blogPostArray[i];
+        addPost(currentPost);
+    }
+});
+
+function addPost(itemValue) {
+    let newEl = document.createElement('p');
+    newEl.textContent = itemValue;
+    newEl.className = 'post-container';
+    postSection.appendChild(newEl);
 }
 
-publishBtn.addEventListener("click", () => {
-    addPost()
-})
+function clearElement() {
+    postSection.innerHTML = '';
+}
+
+function clear() {
+    document.getElementById('input-field').value = '';
+}
+
+publishBtn.addEventListener('click', () => {
+    let inputValue = postInput.value;
+    console.log(inputValue);
+    push(blogPostInDb, inputValue);
+    clear();
+});
