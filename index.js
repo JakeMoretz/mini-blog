88f9d33277c3f38686820e29a2a7eb06dcacf93c
Jake Moretz <jmoretz9090@gmail.com>
2023-11-07 22:46:32 -0500

    got the remove button working with the database, need to work on saving the vote counts in each post

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import {
     ref,
     push,
     onValue,
+    remove,
+    update,
 } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js';
 
 const appSettings = {
@@ -13,99 +15,83 @@ const appSettings = {
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const blogPostInDb = ref(database, 'blogList');
+const postId = [];
 
 const publishBtn = document.querySelector('.publish-btn');
 const postSection = document.querySelector('.container');
 const postInput = document.getElementById('input-field');
 
-
+let upVoteCount = [];
+let downVoteCount = [];
 
 onValue(blogPostInDb, function (snapshot) {
-    //turn object into array
-    let blogPostArray = Object.values(snapshot.val());
     clearElement();
-    //loop through the array
-    for (let i = 0; i < blogPostArray.length; i++) {
-        let currentPost = blogPostArray[i];
-        addPost(currentPost);
-       
-    }
+    snapshot.forEach((childSnapshot) => {
+        let post = childSnapshot.val();
+        let itemValue = post.text;
+        let postUpVoteCount = post.upVotes;
+        let postDownVoteCount = post.downVotes;
+        let id = childSnapshot.key;
+        postId.push(id);
+        addPost(itemValue, postUpVoteCount, postDownVoteCount, id);
+    });
 });
 
-function addPost(itemValue) {
-    let newEl = document.createElement('p');
-    newEl.textContent = itemValue;
+const postRef = ref(database, 'blogList/' + postId);
+
+function addPost(itemValue, postUpVoteCount, postDownVoteCount, id) {
+    let newEl = document.createElement('div');
     newEl.className = 'post-container';
+
+    let postText = document.createElement('p');
+    postText.textContent = itemValue;
+    newEl.appendChild(postText);
+
+    let upVoteDisplay = document.createElement('p');
+    upVoteDisplay.className = 'up-vote-display';
+    upVoteDisplay.textContent = upVoteCount;
+    newEl.appendChild(upVoteDisplay);
+
+    let downVoteDisplay = document.createElement('p');
+    downVoteDisplay.className = 'down-vote-display';
+    downVoteDisplay.textContent = downVoteCount;
+    newEl.appendChild(downVoteDisplay);
+
+    let upVoteImg = document.createElement('img');
+    upVoteImg.src = 'images/up.png';
+    upVoteImg.className = 'up-img';
+    newEl.appendChild(upVoteImg);
+
+    upVoteImg.addEventListener('click', () => {
+        postUpVoteCount++;
+        upVoteDisplay.textContent = postUpVoteCount;
+        upVoteCount.push(postUpVoteCount);
+        console.log(upVoteCount);
+    });
+
+    let downVoteImg = document.createElement('img');
+    downVoteImg.src = 'images/down.png';
+    downVoteImg.className = 'down-img';
+    newEl.appendChild(downVoteImg);
+
+    downVoteImg.addEventListener('click', () => {
+        postDownVoteCount++;
+        downVoteDisplay.textContent = postDownVoteCount;
+        downVoteCount.push(postDownVoteCount);
+    });
+
+    let removeBtn = document.createElement('button');
+    removeBtn.className = 'remove-btn';
+    removeBtn.textContent = 'Remove from feed';
+    newEl.appendChild(removeBtn);
+
+    removeBtn.addEventListener('click', () => {
+        const postRef = ref(database, 'blogList/' + id);
+        remove(postRef);
+    });
+
     postSection.appendChild(newEl);
-
-    downVoteImg()
-    upVoteImg()
-    removeBtn()
-    
 }
-
-let downVoteCount = ""
-let upVoteCount = ""
-
-function downVoteImg() {
-    let postContainer = document.querySelector(".post-container")
-    let downVoteImg = document.createElement("img")
-
-    downVoteImg.src = "images/down.png"
-    downVoteImg.className = "down-img"
-    postContainer.appendChild(downVoteImg)
-    
-    let countDisplay = document.createElement("p");
-    countDisplay.className = "down-vote-display";
-    countDisplay.textContent = downVoteCount;
-    postContainer.appendChild(countDisplay);
-
-    downVoteImg.addEventListener("click", () => {
-        downVoteCount++;
-        countDisplay.textContent = downVoteCount;
-    })
-}
-
-function upVoteImg() {
-    let postContainer = document.querySelector(".post-container")
-    let upVoteImg = document.createElement("img")
-
-    upVoteImg.src = "images/up.png"
-    upVoteImg.className = "up-img"
-    postContainer.appendChild(upVoteImg)
-
-    let countDisplay = document.createElement("p");
-    countDisplay.className = "up-vote-display";
-    countDisplay.textContent = upVoteCount;
-    postContainer.appendChild(countDisplay);
-
-    upVoteImg.addEventListener("click", () => {
-        upVoteCount++;
-        countDisplay.textContent = upVoteCount;
-    })
-
-}
-
-
-function removeBtn() {
-    let postContainer = document.querySelector(".post-container")
-    let removeBtn = document.createElement("button")
-    removeBtn.className="remove-btn"
-    removeBtn.textContent="Remove from feed"
-    postContainer.appendChild(removeBtn)
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 function clearElement() {
     postSection.innerHTML = '';
@@ -117,9 +103,12 @@ function clear() {
 
 publishBtn.addEventListener('click', () => {
     let inputValue = postInput.value;
-    console.log(inputValue);
-    push(blogPostInDb, inputValue);
+
+    push(blogPostInDb, {
+        text: inputValue,
+        upVotes: upVoteCount,
+        downVotes: upVoteCount,
+    });
+
     clear();
 });
-
-
